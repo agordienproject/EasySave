e0cf826075f895cd45706cc25845389503830b3e
lhPierre <pierre.lhomme1@gmail.com>
2024-02-13 22:38:37 +0100

    BackupJobs commands

---
+++ EasySave.WPF/Commands/BackupJobs/DeleteBackupJobCommand.cs
@@ -0,0 +1,27 @@
+﻿using EasySave.Domain.Services;
+using EasySave.WPF.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EasySave.WPF.Commands
+{
+    public class DeleteBackupJobCommand : AsyncCommandBase
+    {
+        private readonly BackupJobsViewModel _backupJobsViewModel;
+        private readonly IBackupJobService _backupJobService;
+
+        public DeleteBackupJobCommand(BackupJobsViewModel backupJobsViewModel, IBackupJobService backupJobService)
+        {
+            _backupJobsViewModel = backupJobsViewModel;
+            _backupJobService = backupJobService;
+        }
+
+        public override Task ExecuteAsync(object parameter)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ EasySave.WPF/Commands/BackupJobs/LoadBackupJobsCommand.cs
@@ -0,0 +1,29 @@
+﻿using EasySave.Domain.Models;
+using EasySave.Domain.Services;
+using EasySave.WPF.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EasySave.WPF.Commands
+{
+    public class LoadBackupJobsCommand : AsyncCommandBase
+    {
+        private readonly BackupJobsViewModel _backupJobsViewModel;
+        private readonly IBackupJobService _backupJobService;
+
+        public LoadBackupJobsCommand(BackupJobsViewModel backupJobsViewModel, IBackupJobService backupJobService)
+        {
+            _backupJobsViewModel = backupJobsViewModel;
+            _backupJobService = backupJobService;
+        }
+
+        public override async Task ExecuteAsync(object parameter)
+        {
+            _backupJobsViewModel.BackupJobs = (List<BackupJob>)await _backupJobService.GetAll();
+        }
+
+    }
+}
